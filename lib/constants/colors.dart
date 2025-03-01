83a25dde82b8781d5c64562e7769c7e60f0a41ce
sachishehan <shehansachintha0@gmail.com>
2025-03-01 09:39:08 +0530

    change color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-const kSubtitleColor = Colors.grey;
-const kCardBgColor = Color.fromARGB(208, 216, 208, 208);
-const kMainWhiteColor = Colors.white;
-const kMainColor = Color(0xff0099FF);
+const kSubtitleColor = Color(0xFFA6A1A1);
+const kMainColor = Color(0xFF000000);
+const kMainWhiteColor = Color(0xFFFFFFFF);
 
-const kMainBlackColor = Color(0xff333333);
-const kMainDarkBlueColor = Color(0xff0147FC);
-const kMainPinkColor = Color(0xffFF0000);
+const kGradientTopColor = Color(0xFF0099FF);
+const kGradientBottomColor = Color(0xFF1444DD);
 
-const kmainLightBlueColor = Color(0xff00F0FF);
-const kMainPurpleColor = Color(0xffEB00FF);
-const kSubPinkColor = Color(0xffFF0068);
+const kCardBackgroundColor = Color(0xFFF5F5F5);
+final kCardButtonColor = Colors.grey[300];
+
+const kMainPinkColor = Colors.pink;
